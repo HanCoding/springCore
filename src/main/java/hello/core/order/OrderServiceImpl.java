f18e427fdde78c0de09d871b509e340ab8f24e60
DeveloperRooney <han_0427@naver.com>
2022-03-21 13:54:38 +0900

    객체 지향 5대 원칙을 지키며 리팩토링

---
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -0,0 +1,25 @@
+package hello.core.order;
+
+import hello.core.discount.DiscountPolisy;
+import hello.core.discount.FixDiscountPolisy;
+import hello.core.discount.RateDiscountPolisy;
+import hello.core.member.Member;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+
+public class OrderServiceImpl implements OrderService{
+
+    // 추상체와 구현체 둘 다 의존하고 있기 때문에 DIP를 위반함
+    // 항상 추상화에만 의존해야 한다
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+//    private final DiscountPolisy discountPolisy = new FixDiscountPolisy();
+    private final DiscountPolisy discountPolisy = new RateDiscountPolisy();
+
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        Member findMember = memberRepository.findById(memberId);
+        int discountPrice = discountPolisy.discount(findMember, itemPrice);
+
+        return new Order(memberId, itemName, itemPrice, discountPrice);
+    }
+}
