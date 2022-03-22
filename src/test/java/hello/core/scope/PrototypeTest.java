2f6d59c6acdebf25e9dc0bfd94b5438b78d0dd19
DeveloperRooney <han_0427@naver.com>
2022-03-22 20:04:32 +0900

    @Scope에 대한 이해

---
+++ src/test/java/hello/core/scope/PrototypeTest.java
@@ -0,0 +1,66 @@
+package hello.core.scope;
+
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.inject.Provider;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PrototypeTest {
+
+    @Test
+    public void findPrototype() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(ClientBean.class, PrototypeBean.class);
+        ClientBean clientBean1 = ac.getBean(ClientBean.class);
+        int count1 = clientBean1.logic();
+        assertThat(count1).isEqualTo(1);
+
+        ClientBean clientBean2 = ac.getBean(ClientBean.class);
+        int count2 = clientBean2.logic();
+        assertThat(count2).isEqualTo(1);
+
+    }
+
+    @RequiredArgsConstructor
+    static class ClientBean {
+
+        private final Provider<PrototypeBean> prototypeBeanObjectProvider;
+
+        public int logic() {
+            PrototypeBean prototypeBean = prototypeBeanObjectProvider.get();
+            prototypeBean.addCount();
+            return prototypeBean.getCount();
+        }
+
+    }
+
+    @Scope("prototype")
+    static class PrototypeBean {
+
+        private int count = 0;
+
+        public void addCount() {
+            count++;
+        }
+
+        public int getCount() {
+            return count;
+        }
+
+        @PostConstruct
+        public void init() {
+            System.out.println("SingletonBean.init");
+        }
+
+        @PreDestroy
+        public void destroy() {
+            System.out.println("SingletonBean.destroy");
+        }
+    }
+}
