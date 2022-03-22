4ee926fe9ab0ac2ec049c9cf0e07d46c0c8b0461
DeveloperRooney <han_0427@naver.com>
2022-03-22 09:57:45 +0900

    @ComponentScan과 @Component에 대한 이해

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -3,7 +3,10 @@ package hello.core.order;
 import hello.core.discount.DiscountPolisy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class OrderServiceImpl implements OrderService{
 
     // 추상체와 구현체 둘 다 의존하고 있기 때문에 DIP를 위반함
@@ -11,6 +14,7 @@ public class OrderServiceImpl implements OrderService{
     private final MemberRepository memberRepository;
     private final DiscountPolisy discountPolisy;
 
+    @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolisy discountPolisy) {
         this.memberRepository = memberRepository;
         this.discountPolisy = discountPolisy;
@@ -22,5 +26,10 @@ public class OrderServiceImpl implements OrderService{
         int discountPrice = discountPolisy.discount(findMember, itemPrice);
 
         return new Order(memberId, itemName, itemPrice, discountPrice);
+    }
+
+    // 테스트용 코드
+    public MemberRepository getMemberRepository() {
+        return memberRepository;
     }
 }
